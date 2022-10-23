721f8028adf32112e2a205834fc3d8eb4f3982f6
LSTM-Kirigaya <1193466151@qq.com>
2022-10-23 14:10:41 +0800

    colorful play.py

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # 基于Luong attention机制的seq2seq2的chatbot
 使用seq2seq架构搭建聊天机器人。 灵感是pytorch的[chatbot_tutorial](https://pytorch.org/tutorials/beginner/chatbot_tutorial.html)
 
-torch官方使用了batch_first=False的数据组织方式，我则使用了batch_first=True的方式（单纯觉得batch_first=True更加符合我的理解）
+torch官方使用了`batch_first=False`的数据组织方式，我则使用了`batch_first=True`的方式（单纯觉得`batch_first=True`更加符合我的理解）
 
 encoder-decoder(Luong attention机制)的图大致如下：
 
 ![image.png](https://i.loli.net/2020/10/08/qaGL8uSsI9PTMir.png)
 
-使用Cornell的电影对话数据集，[链接如下](https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html)
+使用Cornell的电影对话数据集，[链接](https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html) (项目中已经包含数据集，无需额外下载)
 
 ## 训练模型
 ```bash
@@ -17,7 +17,7 @@ python main.py --train --root="data" --device="cuda" --batch_size=64 --save_dir=
 ## 测试性能
 
 ```bash
-python main.py --test --root="data" --state_dict=chatbot/13500_checkpoint.tar
+python main.py --test --root="data" --state_dict=<saved state dict>
 ```
 
 ## 进行推理
@@ -27,3 +27,7 @@ python main.py --test --root="data" --state_dict=chatbot/13500_checkpoint.tar
 ```bash
 python play.py --state_dict=<saved state dict> --show_confidence
 ```
+
+通过命令行来进行简单的交互：
+
+![image.png](https://pic3.zhimg.com/80/v2-e129bc07bf6bc2c5c9ab468c410cb61b_720w.png)
--- play.py
+++ play.py
@@ -1,4 +1,5 @@
 import torch
+from colorama import Style, Fore
 
 from model import EncoderRNN, LuongAttentionDecoderRNN
 from util import parse_arg, GreedySearchDecoder
@@ -31,10 +32,7 @@ if __name__ == "__main__":
                                    padding_idx=voc_dict["pad_token"])
     embedding.load_state_dict(embedding_state_dict)
 
-    encoder = EncoderRNN(hidden_size=hidden_size,
-                         embedding=embedding,
-                         n_layers=encoder_n_layers,
-                         dropout=dropout)
+    encoder = EncoderRNN(hidden_size=hidden_size, embedding=embedding, n_layers=encoder_n_layers, dropout=dropout)
 
     decoder = LuongAttentionDecoderRNN(score_name="dot",
                                        embedding=embedding,
@@ -53,27 +51,22 @@ if __name__ == "__main__":
     # 实例化最终评估模型的实例
     chatbot = GreedySearchDecoder(encoder, decoder)
 
-    caption = '|{}{}{}|'.format(
-        ' ' * 3, "chat begins, press 'q' to quit the conversation", ' ' * 3)
+    caption = '|{}{}{}|'.format(' ' * 3, "chat begins, press 'q' to quit the conversation", ' ' * 3)
     print("-" * len(caption))
     print(caption)
     print("-" * len(caption))
 
     # 进入主循环
     while True:
-        user_dialog = input("User:").lower()
+        user_dialog = input(f" {Fore.YELLOW}User:{Style.RESET_ALL} ").lower()
         if user_dialog == "q" or user_dialog == "quit":
             break
         # word转index， 并将不在词表中的单词替换成unk_token
-        input_seq = [
-            voc_dict["word2index"].get(word, voc_dict["unk_token"])
-            for word in user_dialog.split()
-        ]
+        input_seq = [voc_dict["word2index"].get(word, voc_dict["unk_token"]) for word in user_dialog.split()]
         input_length = torch.tensor([len(input_seq)])
         input_seq = torch.tensor(input_seq).unsqueeze(0)
         # 将index序列输入chatbot，获取回复index序列
-        predict_indexes, predict_confidence = chatbot(input_seq, input_length,
-                                                      MAX_LENGTH)
+        predict_indexes, predict_confidence = chatbot(input_seq, input_length, MAX_LENGTH)
         # 将chatbot回复的index序列转为word，并将代表pad_token和eos_token的index去除
         chatbot_words = []
         for index in predict_indexes:
@@ -82,11 +75,12 @@ if __name__ == "__main__":
             else:
                 chatbot_words.append(voc_dict["index2word"][index])
 
-        print(f"chatbot: {' '.join(chatbot_words)}")
+        reply = ' '.join(chatbot_words).strip()
+        print(Fore.BLUE, "chatbot:", Style.RESET_ALL, reply)
 
         # 将除去pad_token和eos_token的占位符后剩下的字符对应的置信度，保留3位小数输出
         if args.show_confidence:
-            print("chatbot's confidence: {}".format(
-                list(
-                    map(lambda x: str(round(x * 100, 1)) + "%",
-                        predict_confidence[:len(chatbot_words)]))))
+            print(
+                Fore.MAGENTA, "chatbot's confidence: {}".format(
+                    list(map(lambda x: str(round(x * 100, 1)) + "%", predict_confidence[:len(chatbot_words)]))),
+                Style.RESET_ALL)
